eccb148fa2792eabab88e6c05f343abd47661e8d
paolorechia <paolorechia@gmail.com>
2019-02-27 16:32:40 -0300

    gitignore, posfixo, examplos

---
+++ ProjetoBase/teste3.pas
@@ -0,0 +1,7 @@
+program exemplo75 (input, output);
+var m,n :  integer;
+begin    
+  m := 2;
+  n := 3 + 4;
+end.
+
---
+++ ProjetoBase/teste4.pas
@@ -0,0 +1,10 @@
+program exemplo4 (input, output);
+var m,n,o,p :  integer;
+begin    
+  m := 2;
+  n := 3 + 4;
+  o := 6 - 2;
+  p := 8 * 3;
+  m := 12 / 3;
+end.
+
